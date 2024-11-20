3ca5ecb47893b25df1df4343322904258700c520
yusef-r <168993848+yusef-r@users.noreply.github.com>
2024-11-19 19:37:19 -0500

    ugh..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
+import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'SuppleMe - Evolving Wellness. Designed for You.',
   description: 'Supplements designed to support where you are today—and elevate where you’re headed.',
 };
